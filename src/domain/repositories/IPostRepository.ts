08027df9acca47cdd3d1568e8150e4965f39db8d
alisuperpro <alisolorzanove@gmail.com>
2026-04-08 19:24:33 -0400

    create filter post interface

--- src/domain/repositories/IPostRepository.ts
+++ src/domain/repositories/IPostRepository.ts
@@ -1,9 +1,15 @@
 import { Post } from "../entities/Post.entity"
 
+export interface PostFilters {
+  isFeatured?: boolean
+  title?: string
+  authorId?: string
+}
+
 export interface IPostRepository {
   save(post: Post): Promise<Post>
   findById(id: string): Promise<Post | null>
-  findAll(): Promise<Post[]>
+  findAll(filters: PostFilters): Promise<Post[]>
   findBySlug(slug: string): Promise<Post | null>
   findByCategory(categoryId: string): Promise<Post[]>
   delete(id: string): Promise<void>
